2e3167aac26dafda709674cb89ad8f4bf5d0d05e
Iquiji <yt.failerbot.3000@gmail.com>
2018-08-08 14:10:13 +0200

    buffer objekt tut und demo macht x in der mitte

--- demo1.js
+++ demo1.js
@@ -1,9 +1,14 @@
 var m3 = require('./m3.js')
 var graphx = require('./graphx.js')
+floor = Math.floor()
+
+var alpha = [process.stdout.columns/2+5,process.stdout.rows/2+0,1]
 
 var got = graphx.makebuffer(process.stdout.columns,process.stdout.rows," ")
 got.set(process.stdout.columns/2,process.stdout.rows/2,"X")
-got.print()
 while(true){
-	//do something
+	got.set(floor(alpha[0]),floor(alpha[1])," ")
+	alpha = m3.pull(m3.rot(1),alpha)
+	got.set(floor(alpha[0]),floor(alpha[1]),"I")
+	got.print()
 }
