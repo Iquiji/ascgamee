bea91475eec633f946d74cfbd7ae4a03a77e1d4f
Iquiji <yt.failerbot.3000@gmail.com>
2018-08-08 12:08:03 +0200

    graphx tut

--- graphx.js
+++ graphx.js
@@ -1,2 +1,26 @@
 var m3 = require('./m3.js')
 console.log(m3.mmul(m3.unit(),m3.scale(3)))
+var buf = []
+function set_buf(char){
+	for (i= 0; i< 64*24; i++){
+		buf[i] = char                    //make world out of x :) 64x24 ascii
+	}
+}
+
+function print_buf(){
+	for (i= 0;i<buf.length;){
+		var zeile = ""
+		for(x= 0;x<64;x++){			 //print world :)
+			zeile = zeile + buf[i]
+			i += 1
+		}
+		console.log(zeile)
+	}
+}
+
+function set_one(x,y,char){
+	buf[(x+y*64)] = char
+}
+set_buf("#")
+set_one(0,1,"Q")
+print_buf()
